b907699c686234519582cfeb05ef80095e639ace
Roland Haas <rhaas@illinois.edu>
2021-11-20 01:24:56 -0600

    ADIOS: work around bug in HDF5 not detecting version correctly
    for this to happen one needs to:
    
    * have a compiler wrapper
    * user parallel HDF5
    * have a compiler that re-sorts strings
    
    this happens on Cray systems (eg NSERC Perlmutter)

--- src/build.sh
+++ src/build.sh
@@ -42,6 +42,7 @@ ${TAR?} xf ${SRCDIR}/../dist/${NAME}.tar
 echo "ADIOS: Applying patches..."
 pushd ${NAME}
 ${PATCH?} -p1 < ${SRCDIR}/../dist/h5vol_static.patch
+${PATCH?} -p1 < ${SRCDIR}/../dist/hdf5_version.patch
 # Some (ancient but still used) versions of patch don't support the
 # patch format used here but also don't report an error using the exit
 # code. So we use this patch to test for this
