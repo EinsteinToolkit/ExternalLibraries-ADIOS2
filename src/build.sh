a96f1a399eedc899b7c5f11d00d4cb057e05d735
Roland Haas <rhaas@illinois.edu>
2023-03-01 16:08:23 -0600

    ADIOS2: rename to ADIOS2

--- src/build.sh
+++ src/build.sh
@@ -13,33 +13,33 @@ set -e                          # Abort on errors
 
 
 # Set locations
-THORN=ADIOS
-NAME=ADIOS2-2.8.3
+THORN=ADIOS2
+NAME=ADIOS22-2.8.3
 SRCDIR="$(dirname $0)"
 BUILD_DIR=${SCRATCH_BUILD}/build/${THORN}
-if [ -z "${ADIOS_INSTALL_DIR}" ]; then
+if [ -z "${ADIOS2_INSTALL_DIR}" ]; then
     INSTALL_DIR=${SCRATCH_BUILD}/external/${THORN}
 else
     echo "BEGIN MESSAGE"
-    echo "Installing ADIOS into ${ADIOS_INSTALL_DIR}"
+    echo "Installing ADIOS2 into ${ADIOS2_INSTALL_DIR}"
     echo "END MESSAGE"
-    INSTALL_DIR=${ADIOS_INSTALL_DIR}
+    INSTALL_DIR=${ADIOS2_INSTALL_DIR}
 fi
 DONE_FILE=${SCRATCH_BUILD}/done/${THORN}
-ADIOS_DIR=${INSTALL_DIR}
+ADIOS2_DIR=${INSTALL_DIR}
 
-echo "ADIOS: Preparing directory structure..."
+echo "ADIOS2: Preparing directory structure..."
 cd ${SCRATCH_BUILD}
 mkdir build external done 2> /dev/null || true
 rm -rf ${BUILD_DIR} ${INSTALL_DIR}
 mkdir ${BUILD_DIR} ${INSTALL_DIR}
 
 # Build core library
-echo "ADIOS: Unpacking archive..."
+echo "ADIOS2: Unpacking archive..."
 pushd ${BUILD_DIR}
 ${TAR?} xf ${SRCDIR}/../dist/${NAME}.tar
 
-echo "ADIOS: Applying patches..."
+echo "ADIOS2: Applying patches..."
 pushd ${NAME}
 ${PATCH?} -p1 < ${SRCDIR}/../dist/hdf5_version.patch
 # Some (ancient but still used) versions of patch don't support the
@@ -57,58 +57,58 @@ fi
 rm -f .patch_tmp
 popd
 
-echo "ADIOS: Configuring..."
+echo "ADIOS2: Configuring..."
 cd ${NAME}
 
 if [ "${CCTK_DEBUG_MODE}" = yes ]; then
-    ADIOS_BUILD_TYPE=Debug
+    ADIOS2_BUILD_TYPE=Debug
 else
-    ADIOS_BUILD_TYPE=Release
+    ADIOS2_BUILD_TYPE=Release
 fi
 
 # TODO: might be useful to build non-MPI version all the time so that it can
 # run on login nodes
 if [ -n "${HAVE_CAPABILITY_MPI}" ]; then
-    ADIOS2_USE_MPI=ON
+    ADIOS22_USE_MPI=ON
 else
-    ADIOS2_USE_MPI=OFF
+    ADIOS22_USE_MPI=OFF
 fi
 
 if [ -n "${HAVE_CAPABILITY_HDF5}" ]; then
-    ADIOS_HDF5_OPTS="-DHDF5_ROOT=${HDF5_DIR} -DADIOS2_USE_HDF5=ON"
+    ADIOS2_HDF5_OPTS="-DHDF5_ROOT=${HDF5_DIR} -DADIOS22_USE_HDF5=ON"
 else
-    ADIOS_HDF5_OPTS="-DADIOS2_USE_HDF5=OFF"
+    ADIOS2_HDF5_OPTS="-DADIOS22_USE_HDF5=OFF"
 fi
 
-# workaround for https://github.com/ornladios/ADIOS2/issues/3148
+# workaround for https://github.com/ornladios/ADIOS22/issues/3148
 # "Static build, BP5 on SST off fails"
-ADIOS_USE_BP5="${ADIOS_ENABLE_SST}"
+ADIOS2_USE_BP5="${ADIOS2_ENABLE_SST}"
 
-# ADIOS fails with HDF5 1.12 due to H5Oget_info silently having changed its API,
+# ADIOS2 fails with HDF5 1.12 due to H5Oget_info silently having changed its API,
 # so force the minimum knonw API to work
 CXXFLAGS="$CPPFLAGS -DH5Oget_info_vers=2 -DH5O_info_t_vers=1 $CXXFLAGS"
 CFLAGS="$CPPFLAGS -DH5Oget_info_vers=2 -DH5O_info_t_vers=1 $CFLAGS"
 
 # TODO: merge with option list options
 # if [ -n "${HAVE_CAPABILITY_CUDA}" ]; then
-#     ADIOS_USE_CUDA=ON
+#     ADIOS2_USE_CUDA=ON
 # else
-#     ADIOS_USE_CUDAS=OFF
+#     ADIOS2_USE_CUDAS=OFF
 # fi
 
 mkdir build
 cd build
-${CMAKE_DIR:+${CMAKE_DIR}/bin/}cmake -DADIOS2_USE_MPI=${ADIOS2_USE_MPI} ${ADIOS_HDF5_OPTS} -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_INSTALL_LIBDIR=lib  -DBUILD_TESTING=OFF -DADIOS2_BUILD_EXAMPLES=OFF -DADIOS2_USE_Fortran=${ADIOS_ENABLE_FORTRAN} -DADIOS2_USE_Python=OFF -DADIOS2_USE_ZeroMQ=OFF -DADIOS2_USE_PNG=OFF -DADIOS2_USE_BZip2=OFF -DADIOS2_USE_SST=${ADIOS_ENABLE_SST} -DADIOS2_USE_BP5=${ADIOS_USE_BP5} -DADIOS2_USE_CUDA=${ADIOS_ENABLE_CUDA} -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=${ADIOS_BUILD_TYPE} ..
+${CMAKE_DIR:+${CMAKE_DIR}/bin/}cmake -DADIOS22_USE_MPI=${ADIOS22_USE_MPI} ${ADIOS2_HDF5_OPTS} -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_INSTALL_LIBDIR=lib  -DBUILD_TESTING=OFF -DADIOS22_BUILD_EXAMPLES=OFF -DADIOS22_USE_Fortran=${ADIOS2_ENABLE_FORTRAN} -DADIOS22_USE_Python=OFF -DADIOS22_USE_ZeroMQ=OFF -DADIOS22_USE_PNG=OFF -DADIOS22_USE_BZip2=OFF -DADIOS22_USE_SST=${ADIOS2_ENABLE_SST} -DADIOS22_USE_BP5=${ADIOS2_USE_BP5} -DADIOS22_USE_CUDA=${ADIOS2_ENABLE_CUDA} -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=${ADIOS2_BUILD_TYPE} ..
 
-echo "ADIOS: Building..."
+echo "ADIOS2: Building..."
 ${MAKE}
 
-echo "ADIOS: Installing..."
+echo "ADIOS2: Installing..."
 ${MAKE} install
 popd
 
-echo "ADIOS: Cleaning up..."
+echo "ADIOS2: Cleaning up..."
 rm -rf ${BUILD_DIR}
 
 date > ${DONE_FILE}
-echo "ADIOS: Done."
+echo "ADIOS2: Done."
--- src/detect.sh
+++ src/detect.sh
@@ -13,28 +13,28 @@ set -e                          # Abort on errors
 . $CCTK_HOME/lib/make/bash_utils.sh
 
 # Take care of requests to build the library in any case
-ADIOS_DIR_INPUT=$ADIOS_DIR
-if [ "$(echo "${ADIOS_DIR}" | tr '[a-z]' '[A-Z]')" = 'BUILD' ]; then
-    ADIOS_BUILD=1
-    ADIOS_DIR=
+ADIOS2_DIR_INPUT=$ADIOS2_DIR
+if [ "$(echo "${ADIOS2_DIR}" | tr '[a-z]' '[A-Z]')" = 'BUILD' ]; then
+    ADIOS2_BUILD=1
+    ADIOS2_DIR=
 else
-    ADIOS_BUILD=
+    ADIOS2_BUILD=
 fi
 
 # default value for CUDA support
 # TODO: hook up with ExternalLibraries/CUDA
-if [ -z "$ADIOS_ENABLE_CUDA" ] ; then
-    ADIOS_ENABLE_CUDA="no"
+if [ -z "$ADIOS2_ENABLE_CUDA" ] ; then
+    ADIOS2_ENABLE_CUDA="no"
 fi
 
 # default value for FORTRAN support
-if [ -z "$ADIOS_ENABLE_FORTRAN" ] ; then
-    ADIOS_ENABLE_FORTRAN="no"
+if [ -z "$ADIOS2_ENABLE_FORTRAN" ] ; then
+    ADIOS2_ENABLE_FORTRAN="no"
 fi
 
 # default value for SST support
-if [ -z "$ADIOS_ENABLE_SST" ] ; then
-    ADIOS_ENABLE_SST="no"
+if [ -z "$ADIOS2_ENABLE_SST" ] ; then
+    ADIOS2_ENABLE_SST="no"
 fi
 
 ################################################################################
@@ -44,44 +44,44 @@ fi
 # Set up names of the libraries based on configuration variables. Also
 # assign default values to variables.
 # Try to find the library if build isn't explicitly requested
-if [ -z "${ADIOS_BUILD}" -a -z "${ADIOS_INC_DIRS}" -a -z "${ADIOS_LIB_DIRS}" -a -z "${ADIOS_LIBS}" ]; then
-    find_lib ADIOS adios 1 1.0 adios2_core "adios2.h" "$ADIOS_DIR"
+if [ -z "${ADIOS2_BUILD}" -a -z "${ADIOS2_INC_DIRS}" -a -z "${ADIOS2_LIB_DIRS}" -a -z "${ADIOS2_LIBS}" ]; then
+    find_lib ADIOS2 adios 1 1.0 adios2_core "adios2.h" "$ADIOS2_DIR"
 fi
 
-THORN=ADIOS
+THORN=ADIOS2
 
 # configure library if build was requested or is needed (no usable
 # library found)
-if [ -n "$ADIOS_BUILD" -o -z "${ADIOS_DIR}" ]; then
+if [ -n "$ADIOS2_BUILD" -o -z "${ADIOS2_DIR}" ]; then
     echo "BEGIN MESSAGE"
-    echo "Using bundled ADIOS..."
+    echo "Using bundled ADIOS2..."
     echo "END MESSAGE"
-    ADIOS_BUILD=1
+    ADIOS2_BUILD=1
 
     check_tools "tar patch"
     
     # Set locations
     BUILD_DIR=${SCRATCH_BUILD}/build/${THORN}
-    if [ -z "${ADIOS_INSTALL_DIR}" ]; then
+    if [ -z "${ADIOS2_INSTALL_DIR}" ]; then
         INSTALL_DIR=${SCRATCH_BUILD}/external/${THORN}
     else
         echo "BEGIN MESSAGE"
-        echo "Installing ADIOS into ${ADIOS_INSTALL_DIR}"
+        echo "Installing ADIOS2 into ${ADIOS2_INSTALL_DIR}"
         echo "END MESSAGE"
-        INSTALL_DIR=${ADIOS_INSTALL_DIR}
+        INSTALL_DIR=${ADIOS2_INSTALL_DIR}
     fi
-    ADIOS_DIR=${INSTALL_DIR}
+    ADIOS2_DIR=${INSTALL_DIR}
     # Fortran modules may be located in the lib directory
-    ADIOS_INC_DIRS="${ADIOS_DIR}/include ${ADIOS_DIR}/lib"
-    ADIOS_LIB_DIRS="${ADIOS_DIR}/lib"
-    ADIOS_LIBS="adios2_cxx11 adios2_c adios2_core"
-    if [ "$(echo ${ADIOS_ENBABLE_FORTRAN} | tr '[:upper:]' '[:lower:]')" = 'yes' ]; then
-      ADIOS_LIBS="adios2_fortran ${ADIOS_LIBS}"
+    ADIOS2_INC_DIRS="${ADIOS2_DIR}/include ${ADIOS2_DIR}/lib"
+    ADIOS2_LIB_DIRS="${ADIOS2_DIR}/lib"
+    ADIOS2_LIBS="adios2_cxx11 adios2_c adios2_core"
+    if [ "$(echo ${ADIOS2_ENBABLE_FORTRAN} | tr '[:upper:]' '[:lower:]')" = 'yes' ]; then
+      ADIOS2_LIBS="adios2_fortran ${ADIOS2_LIBS}"
     fi
     if [ -n "${MPI_DIR+set}" ]; then
-        ADIOS_LIBS="adios2_cxx11_mpi adios2_c_mpi adios2_core_mpi ${ADIOS_LIBS}"
-        if [ "$(echo ${ADIOS_ENBABLE_FORTRAN} | tr '[:upper:]' '[:lower:]')" = 'yes' ]; then
-          ADIOS_LIBS="adios2_fortran_mpi ${ADIOS_LIBS}"
+        ADIOS2_LIBS="adios2_cxx11_mpi adios2_c_mpi adios2_core_mpi ${ADIOS2_LIBS}"
+        if [ "$(echo ${ADIOS2_ENBABLE_FORTRAN} | tr '[:upper:]' '[:lower:]')" = 'yes' ]; then
+          ADIOS2_LIBS="adios2_fortran_mpi ${ADIOS2_LIBS}"
         fi
     fi
 else
@@ -92,15 +92,15 @@ else
     fi
 fi
 
-if [ -n "$ADIOS_DIR" ]; then
-    : ${ADIOS_RAW_LIB_DIRS:="$ADIOS_LIB_DIRS"}
+if [ -n "$ADIOS2_DIR" ]; then
+    : ${ADIOS2_RAW_LIB_DIRS:="$ADIOS2_LIB_DIRS"}
     # Fortran modules may be located in the lib directory
-    ADIOS_INC_DIRS="$ADIOS_RAW_LIB_DIRS $ADIOS_INC_DIRS"
+    ADIOS2_INC_DIRS="$ADIOS2_RAW_LIB_DIRS $ADIOS2_INC_DIRS"
     # We need the un-scrubbed inc dirs to look for a header file below.
-    : ${ADIOS_RAW_INC_DIRS:="$ADIOS_INC_DIRS"}
+    : ${ADIOS2_RAW_INC_DIRS:="$ADIOS2_INC_DIRS"}
 else
     echo 'BEGIN ERROR'
-    echo 'ERROR in ADIOS configuration: Could neither find nor build library.'
+    echo 'ERROR in ADIOS2 configuration: Could neither find nor build library.'
     echo 'END ERROR'
     exit 1
 fi
@@ -116,23 +116,23 @@ fi
 
 # Pass configuration options to build script
 echo "BEGIN MAKE_DEFINITION"
-echo "ADIOS_BUILD          = ${ADIOS_BUILD}"
-echo "ADIOS_ENABLE_FORTRAN = ${ADIOS_ENABLE_FORTRAN}"
-echo "ADIOS_ENABLE_SST     = ${ADIOS_ENABLE_SST}"
+echo "ADIOS2_BUILD          = ${ADIOS2_BUILD}"
+echo "ADIOS2_ENABLE_FORTRAN = ${ADIOS2_ENABLE_FORTRAN}"
+echo "ADIOS2_ENABLE_SST     = ${ADIOS2_ENABLE_SST}"
 echo "LIBSZ_DIR           = ${LIBSZ_DIR}"
 echo "LIBZ_DIR            = ${LIBZ_DIR}"
-echo "ADIOS_INSTALL_DIR    = ${ADIOS_INSTALL_DIR}"
+echo "ADIOS2_INSTALL_DIR    = ${ADIOS2_INSTALL_DIR}"
 echo "END MAKE_DEFINITION"
 
 # Pass options to Cactus
 echo "BEGIN MAKE_DEFINITION"
-echo "ADIOS_DIR            = ${ADIOS_DIR}"
-echo "ADIOS_ENABLE_FORTRAN = ${ADIOS_ENABLE_FORTRAN}"
-echo "ADIOS_INC_DIRS       = ${ADIOS_INC_DIRS} ${ZLIB_INC_DIRS}"
-echo "ADIOS_LIB_DIRS       = ${ADIOS_LIB_DIRS} ${ZLIB_LIB_DIRS}"
-echo "ADIOS_LIBS           = ${ADIOS_LIBS}"
+echo "ADIOS2_DIR            = ${ADIOS2_DIR}"
+echo "ADIOS2_ENABLE_FORTRAN = ${ADIOS2_ENABLE_FORTRAN}"
+echo "ADIOS2_INC_DIRS       = ${ADIOS2_INC_DIRS} ${ZLIB_INC_DIRS}"
+echo "ADIOS2_LIB_DIRS       = ${ADIOS2_LIB_DIRS} ${ZLIB_LIB_DIRS}"
+echo "ADIOS2_LIBS           = ${ADIOS2_LIBS}"
 echo "END MAKE_DEFINITION"
 
-echo 'INCLUDE_DIRECTORY $(ADIOS_INC_DIRS)'
-echo 'LIBRARY_DIRECTORY $(ADIOS_LIB_DIRS)'
-echo 'LIBRARY           $(ADIOS_LIBS)'
+echo 'INCLUDE_DIRECTORY $(ADIOS2_INC_DIRS)'
+echo 'LIBRARY_DIRECTORY $(ADIOS2_LIB_DIRS)'
+echo 'LIBRARY           $(ADIOS2_LIBS)'
