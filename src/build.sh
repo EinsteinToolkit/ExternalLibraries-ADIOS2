946c29349379615b9909cb44e1280b23ab48a84f
Roland Haas <rhaas@illinois.edu>
2024-10-16 10:48:52 -0500

    ADIOS2: Update to 2.10.1

--- src/build.sh
+++ src/build.sh
@@ -14,7 +14,7 @@ set -e                          # Abort on errors
 
 # Set locations
 THORN=ADIOS2
-NAME=ADIOS2-2.10.0
+NAME=ADIOS2-2.10.1
 SRCDIR="$(dirname $0)"
 BUILD_DIR=${SCRATCH_BUILD}/build/${THORN}
 if [ -z "${ADIOS2_INSTALL_DIR}" ]; then
